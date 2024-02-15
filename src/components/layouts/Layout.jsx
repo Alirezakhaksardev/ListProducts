e2bd68c5eafba8a861d9aeb79b34e175abb36bf7
Alireza <AlirezaKhaksar.freelancer@gmail.com>
2024-02-15 20:31:04 +0330

    Change Layout

--- src/components/layouts/Layout.jsx
+++ src/components/layouts/Layout.jsx
@@ -6,9 +6,9 @@ function Layout({ children }) {
         <>
             <header className='w-full text-center py-3 text-xl font-bold bg-gray-800 rounded-lg text-gray-100'>
                 <h1>
-                    جواب پروژه عملی آزمون 
+                    صفحه محصولات
                     &nbsp;
-                    <span className='text-green-500'>علیرضا خاکسار </span>
+                    <span className='text-green-500'>نام شرکت شما</span>
                 </h1>
             </header>
             <main style={{ minHeight: "80vh" }}>
@@ -17,7 +17,7 @@ function Layout({ children }) {
             <footer className='w-full text-center py-3 font-bold bg-gray-800 rounded-lg text-gray-100'>
                 <p className='flex justify-center items-center'>
                     ساخته شده با &nbsp; <span className='text-red-400'><GoHeartFill /></span> &nbsp;
-                    ایکس کد آکادمی
+                    علیرضا خاکسار
                 </p>
             </footer>
         </>
